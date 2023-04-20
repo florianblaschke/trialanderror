6a097a93e88adce7f3b4ed17d44f3d1a0480e6cb
Florian Blaschke <florianblaschke@web.de>
2023-04-20 21:18:27 +0200

    sliders

--- javascript.js
+++ javascript.js
@@ -28,3 +28,52 @@ bigFatButton.addEventListener("click", () => {
   para.classList.toggle("white");
   paras.classList.toggle("white");
 });
+
+const disco = document.querySelector('[data-js="disco-slider"]');
+const colorChange = document.querySelector('[data-js="color-slider"]');
+const div = document.querySelector('[data-js="div1"]');
+const round = document.querySelector('[data-js="round-slider"]');
+const rotate = document.querySelector('[data-js="rotate-slider"]');
+
+colorChange.addEventListener("input", () => {
+  const color = colorChange.value;
+  div.style.background = `conic-gradient(
+    hsl(${color - 360}, 100%, 50%),
+    hsl(${color - 315}, 100%, 50%),
+    hsl(${color - 270}, 100%, 50%),
+    hsl(${color - 225}, 100%, 50%),
+    hsl(${color - 180}, 100%, 50%),
+    hsl(${color - 135}, 100%, 50%),
+    hsl(${color - 90}, 100%, 50%),
+    hsl(${color - 45}, 100%, 50%),
+    hsl(${color}, 100%, 50%)
+  )`;
+});
+
+round.addEventListener("input", () => {
+  const rounder = round.value;
+  div.style.borderRadius = rounder + "%";
+});
+
+rotate.addEventListener("input", () => {
+  const rotation = rotate.value;
+  div.style.transform = `rotate(${rotation}deg)`;
+});
+
+disco.addEventListener("input", () => {
+  const discopogo = disco.value;
+  div.style.background = `conic-gradient(
+    hsl(${discopogo - 360}, 100%, 50%),
+    hsl(${discopogo - 315}, 100%, 50%),
+    hsl(${discopogo - 270}, 100%, 50%),
+    hsl(${discopogo - 225}, 100%, 50%),
+    hsl(${discopogo - 180}, 100%, 50%),
+    hsl(${discopogo - 135}, 100%, 50%),
+    hsl(${discopogo - 90}, 100%, 50%),
+    hsl(${discopogo - 45}, 100%, 50%),
+    hsl(${discopogo}, 100%, 50%)
+  )`;
+  div.style.transform = `rotateX(${discopogo - 120}deg)`;
+  div.style.transform = `rotateY(${discopogo - 120}deg)`;
+  div.style.transform = `rotateZ(${discopogo}deg)`;
+});
